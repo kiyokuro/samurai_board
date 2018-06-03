a3e3b359834e2d6b1a01bb643c48f48aeb78aa4d
kurosawa <kuro6412@gmail.com>
2018-06-04 01:12:04 +0900

    wip トータルポイント更新

--- app/controllers/task/total_points_controller.rb
+++ app/controllers/task/total_points_controller.rb
@@ -1,17 +1,22 @@
-module Taks
+module Task
   class TotalPointsController < ApplicationController
     def edit
-      board = Task::Board.find parmas[:board_id]
-      @total_point = board.total_points.order(:change_day).limit(1)
+      board = Task::Board.find params[:board_id]
+      @total_point = board.total_points.order(:change_day).take
     end
 
     def update
+      board = Task::Board.find params[:board_id]
       @total_point = board.total_points.order(:change_day).limit(1)
-      @total_point.update(total_point: total_point_params)
+      if @total_point.update(total_point: total_point_params)
+        redirect_to board_path board
+      else
+        render :edit
+      end
     end
 
     def total_point_params
-      parmas.require(:task_total_point).permit(:total_point)
+      params.require(:task_total_point).permit(:total_point)
     end
   end
 end
